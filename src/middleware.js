2bcef9d027e98a29bf4569baba5c4c6642dfc562
ILoveStreaming <trivomarketplace@gmail.com>
2026-05-19 12:40:36 -0300

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -1,1 +1,25 @@
+import { NextResponse } from "next/server";
 
+export function middleware(request) {
+
+  const isLoggedIn =
+    request.cookies.get("admin-auth");
+
+  const isAdminPage =
+    request.nextUrl.pathname.startsWith("/admin");
+
+  if (isAdminPage && !isLoggedIn) {
+
+    return NextResponse.redirect(
+      new URL("/login", request.url)
+    );
+
+  }
+
+  return NextResponse.next();
+
+}
+
+export const config = {
+  matcher: ["/admin/:path*"],
+};
